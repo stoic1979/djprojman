ffad78ac91042f9519d4deb7f582ddc42b6493b2
Navjot Singh <weavebytes@gmail.com>
2015-10-04 00:22:09 +0530

    django form for add project

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -3,3 +3,11 @@ from django import forms
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=32)
     password = forms.CharField(max_length=32, widget=forms.PasswordInput)
+
+class AddProjectForm(forms.Form):
+    title = forms.CharField(max_length=128, 
+            widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter project title here...'}))
+    shortname = forms.CharField(max_length=32,  
+            widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter shortname/alias for project...'}))
+    description = forms.CharField(max_length=2048, 
+            widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter project description here...'}))
