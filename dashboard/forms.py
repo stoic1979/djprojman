c1f17c243e0bab7ebe4e4ae7df9776f555a07fef
Navjot Singh <weavebytes@gmail.com>
2015-10-03 23:53:42 +0530

    password widget in form

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=32)
-    password = forms.CharField(max_length=32)
+    password = forms.CharField(max_length=32, widget=forms.PasswordInput)
