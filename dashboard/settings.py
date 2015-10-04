1e47b50489fb8dc5380e8e582e99661151faa991
Navjot Singh <weavebytes@gmail.com>
2015-10-04 13:32:57 +0530

    adding bare bones of registration app

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'work'
+    'work',
+    'registration'
 )
 
 MIDDLEWARE_CLASSES = (
